c26a48a21b38e17824dd8c98cf6885ff4ec5b457
Sven Over <sven@cord.com>
2022-05-24 16:06:26 +0100

    Fix indentation

--- Formula/spr.rb
+++ Formula/spr.rb
@@ -1,19 +1,19 @@
 class Spr < Formula
-    desc "Submit pull requests for individual, amendable, rebaseable commits to GitHub"
-    homepage "https://github.com/getcord/spr"
-    url "https://github.com/getcord/spr.git",
-        tag:      "v1.2.3",
-        revision: "f48663ae9cbd7eb7dee757c56726818351fb8871"
-    license "MIT"
+  desc "Submit pull requests for individual, amendable, rebaseable commits to GitHub"
+  homepage "https://github.com/getcord/spr"
+  url "https://github.com/getcord/spr.git",
+    tag:      "v1.2.3",
+    revision: "f48663ae9cbd7eb7dee757c56726818351fb8871"
+  license "MIT"
 
-    depends_on "rust" => :build
+  depends_on "rust" => :build
 
-    on_linux do
-        depends_on "pkg-config" => :build
-        depends_on "openssl@1.1"
-    end
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
 
-    def install
-      system "cargo", "install", *std_cargo_args
-    end
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
 end
