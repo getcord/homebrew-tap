6b174f691ffa4063dcc5a2e7bc211ce9f170c872
Sven Over <98333944+sven-of-cord@users.noreply.github.com>
2022-05-27 08:38:24 +0000

    spr: update 1.2.4 bottle.

--- Formula/spr.rb
+++ Formula/spr.rb
@@ -7,10 +7,9 @@ class Spr < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/getcord/homebrew-tap/releases/download/spr-1.2.3"
-    rebuild 1
-    sha256 cellar: :any,                 big_sur:      "e02a8536fbebef6c0d766208072086535fb025a34df019d94fedba0f9c72ff3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "062133dfc8434cd0dcc5777a858bd53a058a446cdfd55b3acdda992919f8d0a4"
+    root_url "https://github.com/getcord/homebrew-tap/releases/download/spr-1.2.4"
+    sha256 cellar: :any,                 big_sur:      "f171c7683bd4d0bb04cd331f03a2fa8c62ccb293a2d70367bb69528c70fc074d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "47a19cb954c14943efc8abaf04d566681ec1e81ddbda08f6a4beafefdd814c7b"
   end
 
   depends_on "rust" => :build
