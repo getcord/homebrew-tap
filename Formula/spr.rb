14351ee4e56af4126257add1374e40c2670aa43d
Sven Over <98333944+sven-of-cord@users.noreply.github.com>
2022-07-18 17:27:39 +0000

    spr: add 1.3.4 bottle.

--- Formula/spr.rb
+++ Formula/spr.rb
@@ -6,6 +6,14 @@ class Spr < Formula
     revision: "1b05ef36f0beb0ff8bdb376bf98ce77a41e41605"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/getcord/homebrew-tap/releases/download/spr-1.3.4"
+    sha256 cellar: :any_skip_relocation, monterey:     "b129726fbbf96238d3479646854c6f8d7a322f14107a84311b19002d5a6214c2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "8738c712487402640a9496b96afb3c9ddf304764d61350137db08d4f9661f5f1"
+    sha256 cellar: :any_skip_relocation, catalina:     "32422a716b36033b4e3c35182375b8aaf03ff68739699bde1042e12d3ef24354"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "719869cd5398d3c32a72b13a42c5ec3d28676da225e01ed5d07d50d3323bfe08"
+  end
+
   depends_on "rust" => :build
   depends_on "git" => :test
   uses_from_macos "zlib"
